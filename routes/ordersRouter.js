e09a498ed1dd3bd6e6ea3053853c5834430a3b82
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-05-31 00:19:57 +0400

    modified:   .gitignore 	new file:   package-lock.json 	modified:   package.json 	modified:   routes/lessonsRouter.js 	modified:   routes/ordersRouter.js 	modified:   server.js

--- routes/ordersRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-ordersRouter = express.Router()
-
-ordersRouter.get('/', (req, res) => {
-    res.send("Welcome to the orders page");
-})
-
-module.exports = ordersRouter;
